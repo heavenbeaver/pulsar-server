033e3b1e46402edfd704d6f978265d46c0a723ea
heavenbeaver <daniilbobritskiy@outlook.com>
2026-03-03 11:05:29 +0500

    users route fix

--- server.ts
+++ server.ts
@@ -7,8 +7,8 @@ import authRouter from './routes/auth.js';
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true
+    // origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    // credentials: true
 }));
 app.use(express.json());
 
